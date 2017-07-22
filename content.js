7c3235adb167d86e2b90ce85f3ee09e86a5e1263
AlexPurhalo <alexpurhalo@gmail.com>
2017-07-22 22:49:09 +0300

    ComponentWillMount-implementation

--- content.js
+++ content.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 class ChildComponent extends React.Component {
 	constructor(props) {
 		super(props)
 		
-		this.state = { message: props.message }
+		this.state = {
+			message: props.message,
+			posts: []
+		}
 	}
 	
 	componentWillReceiveProps(nextProps) {
 		this.setState({ message: nextProps.message })
 	}
 	
+	componentWillMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts/')
+			.then((res) => this.setState({ posts: res.data }))
+	}
 	
 	render() {
 		return (
