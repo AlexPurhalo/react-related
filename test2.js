1a388565b96371d8db2f94ce5ead1a8fc450864a
AlexPurhalo <alexpurhalo@gmail.com>
2017-08-02 12:29:36 +0300

    movement-functions-and-conditions-providing

--- test2.js
+++ test2.js
@@ -1,148 +1,135 @@
+// Data
 const allTunnels = [
 	{
 		id: 1,
-		diamond: true,
-		parent_id: 0,
-		tunnels: [
-			{
-				id: 2,
-				diamond: false,
-				parent_id: 1,
-				tunnels: [
-					{
-						id: 3,
-						diamond: true,
-						parent_id: 2
-					},
-					{
-						id: 4,
-						diamond: false,
-						parent_id: 2,
-					}
-				]
-			}
-		]
-	},
-	{
-		id: 5,
 		diamond: false,
 		parent_id: 0,
 		tunnels: [
 			{
-				id: 6,
+				id: 2,
 				diamond: true,
-				parent_id: 5,
+				parent_id: 1,
+				tunnels: [
+					{
+						id: 3,
+						diamond: false,
+						parent_id: 2,
+						tunnels: [
+							{
+								id: 4,
+								diamond: false,
+								parent_id: 3,
+								tunnels: [
+									{ id: 5, diamond: false, parent_id: 4 },
+									{ id: 6, diamond: true, parent_id: 4 },
+									{ id: 7, diamond: false, parent_id: 4 }
+								]
+							},
+							{
+								id: 8, diamond: false, parent_id: 3
+							},
+							{
+								id: 9,
+								diamond: false,
+								parent_id: 3,
+								tunnels: [
+									{ id: 10, diamond: false, lvl: 4, parent_id: 9 }
+								]
+							}
+						]
+					},
+					{
+						id: 11, diamond: false, parent_id: 2
+					}
+				]
+			},
+			{
+				id: 12,
+				diamond: true,
+				parent_id: 1,
+				tunnels: [
+					{
+						id: 13, diamond: false, parent_id: 12
+					}
+				]
+			},
+			{
+				id: 14,
+				diamond: false,
+				parent_id: 1
 			}
 		]
 	},
 	{
-		id: 7,
-		diamond: true,
-		parent_id: 0,
-		tunnels: [
-			{
-				id: 8,
-				diamond: false,
-				parent_id: 7
-			}
-		]
+		id: 15,
+		diamond: false,
+		parent_id: 0
 	}
 ]
 
-// const isLastSearch = (lvls) => {
-// 	const currTunnelIndex = lvls[1]
-// 	const lastTunnelIndex = initTunnel['tunnels'].length-1
-//
-// 	return currTunnelIndex >= lastTunnelIndex
-// }
 
-// if (lvls.length >= 2 && isLastSearch(lvls)) return 1
-
-// if (!isTunnelExist(tunnel)) {
-// 	lvls = backToPrevLvl(lvls)
-// 	lvls = goToNextTunnel(lvls)
-// 	tunnel = lvls[lvls.length-1]
-//
-// 	return findTunnel(tunnel, lvls)
-// }
-
-// if (parId) {
-// 	if (isParentTunnel(tunnel, parId)) return tunnel
-// }
-
-const checkTunnel = (initTunnel, id) => {
+// Realization
+const findTunnelById = (initTunnel, id) => {
+	// Actions
+	const goDown = (p) => {
+		p[p.length] = 0
+		return p
+	}
+	
+	const goBack = (p) => p.slice(0, p.length-1)
+	
+	const goToNext = (p) => {
+		p[p.length-1]+=1
+		return p
+	}
+	
 	// Conditions
-	const isExactTunnel    = (tunnel, currId) => tunnel['id'] === currId
-	const isParentTunnel  = (tunnel, parId) => tunnel['id'] === parId
-	const isTunnelExist    = (tunnel) => tunnel
-	const hasSubTunnels    = (tunnel) => tunnel['tunnels']
+	const isExactTunnel = (tunnel, currId) => tunnel['id'] === currId
+	const hasTunnels    = (tunnel) => tunnel['tunnels']
+	const isTunnelExist = (parTunnel, position) => parTunnel['tunnels'][position[position.length-1]]
 	
-	const hasTunnelsBehind = (tunnel, lvls) => {
-		const lastTunnelIndex = tunnel['tunnels'].length-1
-		const currTunnelIndex = lvls[lvls.length-1]
+	// Function
+	const findTunnel = (currTunnel, currId, position) => {
+		if (isExactTunnel(currTunnel, currId)) return currTunnel
 		
-		return  currTunnelIndex < lastTunnelIndex
-	}
-	
-	const findTunnel = (tunnel, lvls, currId) => {
-		// console.log(`id: ${tunnel['id']}, [${lvls}]`)
-		if (!isTunnelExist(tunnel)) {
-			lvls = backToPrevLvl(lvls)
-			lvls = goToNextTunnel(lvls)
+		position = goDown(position)
+		
+		if (hasTunnels(currTunnel) && isTunnelExist(currTunnel, position))
+			return findTunnel(
+				currTunnel['tunnels'][position[position.length-1]],
+				currId,
+				position
+			)
+		
+		const findParentsNextTunnel = (tunnel, p) => {
+			const parentTunnel = findTunnel(initTunnel, tunnel['parent_id'], [0])
 			
-			const parent = findTunnel(initTunnel, [0], 0)
+			p = goBack(p)
+			p = goToNext(p)
 			
+			if (isTunnelExist(parentTunnel, p))
+				return findTunnel(parentTunnel['tunnels'][p[p.length-1]], currId, p)
 			
-			return parent['tunnels'][lvls[lvls.length-1]]
+			return findParentsNextTunnel(parentTunnel, p)
 		}
 		
-		if (isExactTunnel(tunnel, currId)) return tunnel
-		
-		if (hasSubTunnels(tunnel)) {
-			lvls = addLvl(lvls)
-			tunnel = tunnel['tunnels'][lvls[lvls.length-1]]
-			
-			return findTunnel(tunnel, lvls, currId)
-		}
-		
-		
-		const parentTunnel = findTunnel(initTunnel, [0], tunnel['parent_id'])
-		// console.log(hasTunnelsBehind(parentTunnel, lvls))
-		
-		if (hasTunnelsBehind(parentTunnel, lvls)) {
-			lvls = goToNextTunnel(lvls)
-			tunnel = parentTunnel['tunnels'][lvls[lvls.length-1]]
-			
-			return findTunnel(tunnel, lvls, currId)
-		}
-		
-		lvls = backToPrevLvl(lvls)
-		lvls = goToNextTunnel(lvls)
-		
-		const hugeParentTunnel = findTunnel(initTunnel, [0], parentTunnel['parent_id'])
-		tunnel = hugeParentTunnel['tunnels'][lvls[lvls.length-1]]
-		return findTunnel(tunnel, lvls, currId)
+		return findParentsNextTunnel(currTunnel, position)
 	}
 	
-	// Methods
-	const backToPrevLvl  = (lvls) => lvls.slice(0, lvls.length-1)
-	
-	const goToNextTunnel = (lvls) => {
-		lvls[lvls.length-1] += 1
-		return lvls
-	}
-	
-	const addLvl = (lvls) => {
-		lvls[lvls.length] = 0
-		return lvls
-	}
-	
-	return findTunnel(initTunnel, [0], id)
+	return findTunnel(initTunnel, id, [0])
 }
 
-console.log(checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 5))
-// checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 3)
-// checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 3)
-// checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 4)
-// checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 5)
-// checkTunnel({ id: 0, diamond: false,tunnels: allTunnels }, 8)
+// Find tunnel bellow
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 0))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 1))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 3))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 5))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 10))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 13))
+
+// Find tunnel behind
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 7))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 15))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 11))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 12))
+console.log(findTunnelById({ id: 0, tunnels: allTunnels}, 15))
